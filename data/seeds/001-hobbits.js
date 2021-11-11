7394c7f0f59ff2acd19d8c95a0eefa5ba6b62495
Ladrillo <gabriel@cabrejas.net>
2021-11-11 19:12:31 +0100

    wip

--- data/seeds/001-hobbits.js
+++ data/seeds/001-hobbits.js
@@ -1,3 +1,22 @@
+const hobbits = [
+  {
+    "id": 1,
+    "name": "sam"
+  },
+  {
+    "id": 2,
+    "name": "frodo"
+  },
+  {
+    "id": 3,
+    "name": "pippin"
+  },
+  {
+    "id": 4,
+    "name": "merry"
+  }
+])
+
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries and resets ids
   return knex('hobbits')
